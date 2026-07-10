eceb0a8f08dd9594cf271023b60acb58fbe877c5
chattsayan <chatterjeesayan730@gmail.com>
2026-07-10 09:25:55 +0530

    chore: initialize Express server with basic setup

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+dns.setDefaultResultOrder("ipv4first");
+
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const studentRoutes = require("./routes/studentRoutes");
+const errorHandler = require("./middleware/errorHandler");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Student Management API is running...");
+});
+
+app.use("/api/students", studentRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
